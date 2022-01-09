bdd997d01ae229242678d755862e7947bfcf4031
kofoworola <adenekankofoworola@gmail.com>
2022-01-09 13:52:14 +0100

    modify filter year dropdown

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ const App = () => {
 
   return (
     <div style={AppStyle}>
-
       <NewExpense onAddExpense={handleAddExpense} />
       <Expenses 
         item={expense} 
--- src/components/Expenses/ExpenseFilter/expensefilter.jsx
+++ src/components/Expenses/ExpenseFilter/expensefilter.jsx
@@ -5,17 +5,20 @@ const ExpenseFilter = (props) => {
 
   const handleChange = (e) => {
     props.filterdValue(e.target.value)
+    console.log(e.target.value, 'vallll')
   }
+
+  console.log(props.selected)
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select onChange={handleChange} value={props.selected}>
-          <option value='2022'>2019</option>
-          <option value='2021'>2020</option>
-          <option value='2020'>2021</option>
-          <option value='2019'>2012</option>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
         </select>
       </div>
   </div>
